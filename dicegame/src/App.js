c9a4bd246d7d09051caf85598d027608f12a2940
CJewon <112820744+CJewon@users.noreply.github.com>
2024-05-17 22:57:14 +0900

    diceGame html, css 작업

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import BlueDice01 from "./img/dice-blue-1.svg";
+import RedDice01 from "./img/dice-red-1.svg";
+import css from "./css/App.css";
+import logo from "./img/logo.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <div>
+        <img class="App-logo" src={logo} alt="주사위게임 로고" />
+        <h1 class="App-title">주사위게임</h1>
+        <div>
+          <button class="Button blue App-button">던지기</button>
+          <button class="Button red App-button">처음부터</button>
+        </div>
+      </div>
+      <div class="App-boards">
+        <div class="Board App-board">
+          <h2 class="Board-heading">나</h2>
+          <img class="Dice" src={BlueDice01} alt="1" />
+          <h2 class="Board-heading">기록</h2>
+          <p></p>
+          <h2 class="Board-heading">총점</h2>
+          <p>0</p>
+        </div>
+        <div class="Board App-board">
+          <h2 class="Board-heading">상대</h2>
+          <img class="Dice" src={RedDice01} alt="1" />
+          <h2 class="Board-heading">기록</h2>
+          <p></p>
+          <h2 class="Board-heading">총점</h2>
+          <p>0</p>
+        </div>
+      </div>
     </div>
   );
 }
